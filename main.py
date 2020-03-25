c458456a8108496f571a988edeeea881518a636f
vadim.trunin <demptdmf@bk.ru>
2020-03-25 18:17:38 +0300

    My 1st git-commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import requests   №импорт оранжевый, а ниже нет
+import requests
 import json
 
 url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(url)
 print('My', response, 'so its ok, and Hello World! :)')
 
-# Почему-то не всегда создаются импорты и требуется настраивать под каждый проект их повторно, скрин: https://prnt.sc/rmfnjs
+#1. Почему-то не всегда создаются импорты и требуется настраивать под каждый проект их повторно, скрин: https://prnt.sc/rmfnjs
+#2. почему-то в гит улетела папка .idea, хотя отмечался только мейн.ру — как удалить это из гита?
