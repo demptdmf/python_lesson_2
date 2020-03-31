59f61964eb50da8028001c7d09799416ff404934
vadim.trunin <demptdmf@bk.ru>
2020-04-01 01:53:45 +0300

    delete .idea

--- main.py
+++ main.py
@@ -6,3 +6,24 @@ import json
 url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(url)
 print('My', response, 'so its ok, and Hello World! :)')
+
+# Добавить папку в игнор-лист гита
+echo
+'.idea' >>.gitignore
+
+# Удалить папку из стейджинга
+git
+rm - r - -cached.idea
+
+# Добавить файл в гит
+git
+add.gitignore
+
+# Зафиксировать изменения
+git
+commit - m
+'Удалил папку .idea из репозитория'
+
+# Запушить в репу
+git
+push
